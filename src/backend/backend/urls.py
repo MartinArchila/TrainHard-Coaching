2a847c03d9fccfac11113ba4ada412692b812818
Martin Archila <archilamartin12@gmail.com>
2026-02-15 18:45:29 -0500

    Update
    Updated serailizers for correct updating and modification of coaches.
    
    Cleaned up urls for simpler understanding
    Update

--- src/backend/backend/urls.py
+++ src/backend/backend/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("auth/token/", TokenObtainPairView.as_view(), name="get_token"),
+    # path('admin/', admin.site.urls),
+    path("login/", TokenObtainPairView.as_view(), name="get_token"),
     path("auth/refresh/", TokenRefreshView.as_view(),name="refresh_token"),
-    path("auth/", include("coach.urls")),
+    path("coach/", include("coach.urls")),
 ]
--- src/backend/coach/urls.py
+++ src/backend/coach/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import CoachCreateView, CoachListView, CoachDetailView, CoachUpdateDeleteView
 
 urlpatterns = [
-    path("coach/", CoachCreateView.as_view(), name="coach-create"),
-    path("coaches/",CoachListView.as_view(), name = "coach_list"),
-    path("coach/<uuid:pk>/", CoachDetailView.as_view(), name="coach_details"),
-    path("coaches/<uuid:id>/",CoachUpdateDeleteView.as_view(), name="coach-update-delete"),
+    path("create/", CoachCreateView.as_view(), name="coach-create"),
+    path("list/",CoachListView.as_view(), name = "coach_list"),
+    path("details/<uuid:pk>/", CoachDetailView.as_view(), name="coach_details"),
+    path("edit/<uuid:id>/",CoachUpdateDeleteView.as_view(), name="coach-update-delete"),
 ]
